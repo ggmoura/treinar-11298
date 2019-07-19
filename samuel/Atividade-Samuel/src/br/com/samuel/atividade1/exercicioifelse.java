6688bd44249a3a772418818c3f668746e120825a
ADM <hackbras>
2019-07-19 19:50:30 -0300

    Atividade Samuel

---
+++ samuel/Atividade-Samuel/src/br/com/samuel/atividade1/exercicioifelse.java
@@ -0,0 +1,15 @@
+package br.com.samuel.atividade1;
+
+public class exercicioifelse {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		if (a == 1);
+		System.out.println(" a é igual a 1: ");
+		
+			
+	}
+
+}
---
+++ samuel/Atividade-Samuel/src/br/com/samuel/atividade1/operadoreslogicos.java
@@ -0,0 +1,9 @@
+package br.com.samuel.atividade1;
+
+public class operadoreslogicos {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
