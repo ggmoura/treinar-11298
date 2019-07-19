32aab7960d0761b529b1ead3f5e77b6127a4f241
Treinar <Treinar@MICRO49>
2019-07-19 19:51:08 -0300

    obrigatoriamente =s

---
+++ humberto/atividade/aula19-07/src/aula1907/ClassesWrapper.java
@@ -0,0 +1,54 @@
+package aula1907;
+
+
+
+public class ClassesWrapper {
+	
+	public static void main(String[] args) {
+		
+		int i = 100;
+		int j = 100;
+		int m = 1000;
+		int n = 1000;
+		
+		Integer a = 100;
+		Integer b = 100;
+		Integer x = 1000;
+		Integer  y = 1000;
+		
+		
+		System.out.println(i == j);
+		System.out.println(m == n);
+		System.out.println(a == n);
+		System.out.println(x == y);
+		
+		String nome = "berison";
+		String apelido = "berison";
+				
+				System.out.println(nome == apelido);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+	}
+
+}
