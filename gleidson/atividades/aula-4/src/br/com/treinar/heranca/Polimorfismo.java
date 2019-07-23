64e068df04c1cff282c0d12ad96455a0e6ca20be
ADM <ADM@MICRO137>
2019-07-22 22:15:06 -0300

    correcao de bug
    melhorar a interface com o usuario
    
    solicitar a digitacao do nome

--- gleidson/atividades/aula-4/src/br/com/treinar/heranca/Polimorfismo.java
+++ gleidson/atividades/aula-4/src/br/com/treinar/heranca/Polimorfismo.java
@@ -8,15 +8,17 @@ public class Polimorfismo {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite 1 Mulher 2 Homem");
+		System.out.print("Digite 1 Mulher 2 Homem: ");
 		Integer opcao = sc.nextInt();
-		//ternario 
+		// remover enter deixado na leitura do inteiro
+		sc.nextLine();
+		// ternario
 		Pessoa p = opcao == 1 ? new Mulher() : new Homem();
+		System.out.print("Nome: ");
 		p.setNome(sc.nextLine());
 		p.respirar();
 
 		sc.close();
 	}
-	
 
 }
